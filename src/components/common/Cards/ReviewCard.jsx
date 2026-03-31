8a5e90799e1f9d5416aef7a78ce407f2059d8a84
Antonela Borgogno <antonelaborgogno@gmail.com>
2026-03-31 12:58:37 -0300

    fix error de importacion

--- src/components/common/Cards/ReviewCard.jsx
+++ src/components/common/Cards/ReviewCard.jsx
@@ -1,6 +1,3 @@
-<<<<<<< fix-info
-import { Box, Typography, Stack, Rating, Avatar, useTheme, alpha, Skeleton } from '@mui/material';
-=======
 import {
     Box,
     Typography,
@@ -11,7 +8,6 @@ import {
     alpha,
     Skeleton,
 } from '@mui/material';
->>>>>>> dev
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 import PlaceIcon from '@mui/icons-material/Place';
 
